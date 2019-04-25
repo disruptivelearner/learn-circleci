3abeaccf13fd67fc8cfa2deb8efc19b0a2170aba
Disruptive Learner <ex@example.com>
2019-04-25 13:28:47 -0400

    Run go program

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/disruptivelearner/stringutils"
+)
+
+func main() {
+	fmt.Println("Hello, Cool World")
+	//fmt.Println(stringutils.Lower("Hello, Cool World"))
+}
---
+++ stringutils.go
@@ -0,0 +1,9 @@
+package stringutils
+
+func Uppers(s string) string {
+	return string.ToUpper(s)
+}
+
+func Lowers(s string) string {
+	return string.ToLower(s)
+}
